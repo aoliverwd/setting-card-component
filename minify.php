1f0a3951fc65596f5272cc0cfa657e810e3d299c
Alex Oliver <alex.oliver@avantiseducation.com>
2022-11-25 17:15:05 +0000

    Build latest assets

--- minify.php
+++ minify.php
@@ -4,29 +4,38 @@
 $path = __DIR__ . '/dist/';
 $src = __DIR__ . '/src/';
 
-// $components = [
-//     file_get_contents(__DIR__ . '/src/settings-card.js'),
-//     file_get_contents(__DIR__ . '/src/form-input.js'),
-//     file_get_contents(__DIR__ . '/src/add-rows.js')
-// ];
+$components = [
+    __DIR__ . '/src/settings-card.js',
+    __DIR__ . '/src/form-input.js',
+    __DIR__ . '/src/add-rows.js'
+];
 
-// foreach (scandir($path) as $file) {
-//     $fileinfo = pathinfo($path . $file);
-//     if ($fileinfo['extension'] === 'css') {
-//         $component = preg_replace(
-//             '/<link rel="stylesheet" href="\${style_path}' . $fileinfo['basename'] . '">/',
-//             '<style>' . file_get_contents($path . $file) . '</style>',
-//             $component
-//         );
-//     }
-// }
+$components = array_map(function ($file) use ($path) {
+    if (file_exists($file)) {
+        $file_info = pathinfo($file);
+        $component = file_get_contents($file);
 
-// file_put_contents($path . 'settings-card.js', $component);
+        foreach (scandir($path) as $file) {
+            $fileinfo = pathinfo($path . $file);
+            if ($fileinfo['extension'] === 'css') {
+                $component = preg_replace(
+                    '/<link rel="stylesheet" href="\${style_path}' . $fileinfo['basename'] . '">/',
+                    '<style>' . file_get_contents($path . $file) . '</style>',
+                    $component
+                );
+            }
+        }
 
-exec('minify ' . $src . 'settings-card.js > ' . $path . 'settings-card.js');
-exec('minify ' . $src . 'form-input.js > ' . $path . 'form-input.js');
-exec('minify ' . $src . 'add-rows.js > ' . $path . 'add-rows.js');
+        file_put_contents($path . $file_info['basename'], $component);
+    }
 
-// unlink($path . 'settings-card.js');
-// unlink($path . 'form-input.js');
-// unlink($path . 'add-rows.js');
+    return false;
+}, $components);
+
+exec('minify ' . $path . 'settings-card.js > ' . $path . 'settings-card.min.js');
+exec('minify ' . $path . 'form-input.js > ' . $path . 'form-input.min.js');
+exec('minify ' . $path . 'add-rows.js > ' . $path . 'add-rows.min.js');
+
+unlink($path . 'settings-card.js');
+unlink($path . 'form-input.js');
+unlink($path . 'add-rows.js');
