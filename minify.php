86ec5240c7129eafdc44bfb027810f7d4c490d2c
Alex Oliver <alex.oliver@avantiseducation.com>
2022-11-25 16:32:48 +0000

    Build components

--- minify.php
+++ minify.php
@@ -2,20 +2,31 @@
 <?php
 
 $path = __DIR__ . '/dist/';
-$component = file_get_contents(__DIR__ . '/src/settings-card.js');
+$src = __DIR__ . '/src/';
 
-foreach (scandir($path) as $file) {
-    $fileinfo = pathinfo($path . $file);
-    if ($fileinfo['extension'] === 'css') {
-        $component = preg_replace(
-            '/<link rel="stylesheet" href="\${style_path}' . $fileinfo['basename'] . '">/',
-            '<style>' . file_get_contents($path . $file) . '</style>',
-            $component
-        );
-    }
-}
+// $components = [
+//     file_get_contents(__DIR__ . '/src/settings-card.js'),
+//     file_get_contents(__DIR__ . '/src/form-input.js'),
+//     file_get_contents(__DIR__ . '/src/add-rows.js')
+// ];
 
-file_put_contents($path . 'settings-card.js', $component);
+// foreach (scandir($path) as $file) {
+//     $fileinfo = pathinfo($path . $file);
+//     if ($fileinfo['extension'] === 'css') {
+//         $component = preg_replace(
+//             '/<link rel="stylesheet" href="\${style_path}' . $fileinfo['basename'] . '">/',
+//             '<style>' . file_get_contents($path . $file) . '</style>',
+//             $component
+//         );
+//     }
+// }
 
-exec('minify ' . $path . 'settings-card.js > ' . $path . 'settings-card.min.js');
-unlink($path . 'settings-card.js');
+// file_put_contents($path . 'settings-card.js', $component);
+
+exec('minify ' . $src . 'settings-card.js > ' . $path . 'settings-card.js');
+exec('minify ' . $src . 'form-input.js > ' . $path . 'form-input.js');
+exec('minify ' . $src . 'add-rows.js > ' . $path . 'add-rows.js');
+
+// unlink($path . 'settings-card.js');
+// unlink($path . 'form-input.js');
+// unlink($path . 'add-rows.js');
